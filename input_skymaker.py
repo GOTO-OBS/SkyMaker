e5374f6f08953a7804920f59b985dc6155046545
James Mullaney <Home@JRMBook.lan>
2017-03-23 20:53:25 +0000

    Added facility to specify any (RA,Dec).

--- input_skymaker.py
+++ input_skymaker.py
@@ -10,80 +10,104 @@ from astropy import coordinates as coord
 from astroquery.sdss import SDSS
 from astroquery.vizier import Vizier
 
-#Define the columns to get from UCAC, magnitude filter and set unlimited rows:
-v = Vizier(columns=['_RAJ2000', '_DEJ2000', 'Vmag'],column_filters={"Vmag":"<16"})
-v.ROW_LIMIT=-1
+import matplotlib.pyplot as plt
 
-#Query UCAC4
-result = v.query_region(\
-        coord.SkyCoord(ra=0.0,dec=0.0,unit=(u.deg,u.deg),frame='icrs'),\
-        width='3d',catalog=['UCAC4'])[0]
+def makelist(ra, dec, fname):
+    #Define size of camera in pixels:
+    xsi = 8176.
+    ysi = 6132.
 
-#Extract values from UCAC4
-xs = result['_RAJ2000']
-ys = result['_DEJ2000']
-ms = result['Vmag']
+    #Corresponding size in degs:
+    wid = xsi*1.24/3600.
+    hei = ysi*1.24/3600.
 
-#Query SDSS:
-query = "SELECT p.objid, p.ra, p.dec, p.g, "+\
-  "p.deVRad_g, p.deVPhi_g, p.deVAB_g, "+\
-  "p.type, p.flags_g, (flags & dbo.fPhotoFlags('SATURATED')) as SAT "+\
-  "FROM PhotoPrimary AS p "+\
-  "WHERE  p.ra BETWEEN -1.4 AND 1.4 AND "+\
-  "p.dec BETWEEN -1.1 AND 1.1 AND "+\
-  "p.g BETWEEN 16 AND 21 AND "+\
-  "p.htmid*37 & 0x000000000000FFFF < (650 * 10)"
-res = SDSS.query_sql(query)
+    #Define the columns to get from UCAC, magnitude filter and set unlimited rows:
+    v = Vizier(columns=['_RAJ2000', '_DEJ2000', 'Vmag'], column_filters={"Vmag":"<16"})
+    v.ROW_LIMIT=-1
 
-#Extract the values from the SDSS table:
-x = res['ra']
-y = res['dec']
-m = res['g']
-t = res['type']
-a = res['deVRad_g']
-ba = res['deVAB_g']
-pa = res['deVPhi_g']
+    #Query UCAC4
+    result = v.query_region(\
+                            coord.SkyCoord(ra=ra, dec=dec,\
+                            unit=(u.deg, u.deg),\
+                            frame='icrs'),\
+                            height=str(hei+1)+"d", width=str(wid+1)+"d",\
+                            catalog=["UCAC4"])[0]
 
-#Remove the saturated sources:
-ma = np.ma.masked_where(res['SAT']!=0, res['SAT'])
-x = x[~ma.mask].copy()
-y = y[~ma.mask].copy()
-m = m[~ma.mask].copy()
-t = t[~ma.mask].copy()
-a = a[~ma.mask].copy()
-ba = ba[~ma.mask].copy()
-pa = pa[~ma.mask].copy()
+    #Extract values from UCAC4
+    xs = result['_RAJ2000']
+    ys = result['_DEJ2000']
+    ms = result['Vmag']
 
-#Generate WCS to convert (RA,Dec) to (x,y)
-w = wcs.WCS(naxis=2)
-w.wcs.crpix = [4088, 3066]
-w.wcs.cdelt = np.array([3.444e-4, 3.444e-4])
-w.wcs.crval = [0., 0.] #Pointing position of telescope.
-w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+    #For SDSS, need to calculate bounding box:
+    declim = dec+np.array([-hei,hei])/2.
+    declim_r = np.pi*declim/180.
+    wid_r = np.pi*wid/180.
+    d_lim_r = 2.*np.arcsin(np.sin(wid_r/4.)/np.cos(declim_r))
+    ralim = ra+np.array([-1,1])*np.max(180.*d_lim_r/np.pi)
+    
+    #Query SDSS:
+    query = "SELECT p.objid, p.ra, p.dec, p.g, "+\
+    "p.deVRad_g, p.deVPhi_g, p.deVAB_g, "+\
+    "p.type, p.flags_g, (flags & dbo.fPhotoFlags('SATURATED')) as SAT "+\
+    "FROM PhotoPrimary AS p "+\
+    "WHERE  p.ra BETWEEN "+str(ralim[0])+" AND "+str(ralim[1])+" AND "+\
+    "p.dec BETWEEN "+str(declim[0])+" AND "+str(declim[1])+" AND "+\
+    "p.g BETWEEN 16 AND 21 AND "+\
+    "p.htmid*37 & 0x000000000000FFFF < (650 * 10)"
+    res = SDSS.query_sql(query)
 
-pixcrd = np.column_stack((x,y))
-pixcrds = np.column_stack((xs,ys))
-
-world =  w.wcs_world2pix(pixcrd, 1)
-worlds =  w.wcs_world2pix(pixcrds, 1)
-
-x =  world[:,0]
-y =  world[:,1]
-
-xs =  worlds[:,0]
-ys =  worlds[:,1]
-
-#Write SDSS to file, if type==6, then it's a star, otherwise galaxy:
-myfile= open('list.list','w')
-for i in range(0,x.size):
-    if t[i] == 6:
+    #Extract the values from the SDSS table:
+    x = res['ra']
+    y = res['dec']
+    m = res['g']
+    t = res['type']
+    a = res['deVRad_g']
+    ba = res['deVAB_g']
+    pa = res['deVPhi_g']
+    
+    #Remove the saturated sources:
+    ma = np.ma.masked_where(res['SAT']!=0, res['SAT'])
+    x = x[~ma.mask].copy()
+    y = y[~ma.mask].copy()
+    m = m[~ma.mask].copy()
+    t = t[~ma.mask].copy()
+    a = a[~ma.mask].copy()
+    ba = ba[~ma.mask].copy()
+    pa = pa[~ma.mask].copy()
+    
+    #Generate WCS to convert (RA,Dec) to (x,y)
+    w = wcs.WCS(naxis=2)
+    w.wcs.crpix = [4088, 3066]
+    w.wcs.cdelt = np.array([3.444e-4, 3.444e-4])
+    w.wcs.crval = [ra, dec] #Pointing position of telescope.
+    w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+    
+    pixcrd = np.column_stack((x,y))
+    pixcrds = np.column_stack((xs,ys))
+    
+    world =  w.wcs_world2pix(pixcrd, 1)
+    worlds =  w.wcs_world2pix(pixcrds, 1)
+    
+    x =  world[:,0]
+    y =  world[:,1]
+    
+    xs =  worlds[:,0]
+    ys =  worlds[:,1]
+    
+    #Write SDSS to file, if type==6, then it's a star, otherwise galaxy:
+    myfile= open(fname+'.list','w')
+    for i in range(0,x.size):
+        if t[i] == 6:
             myfile.write((str(100)+' '+str(x[i])+' '+str(y[i])+' '+str(m[i])+'\n'))
-    else:
-        myfile.write((str(200)+' '+str(x[i])+' '+str(y[i])+' '+str(m[i])+' ' +\
-                      str(0)+' ' +str(0)+' ' +str(0)+' ' +str(0)+' ' + str(a[i])+' '+\
-                      str(a[i]*ba[i])+' '+str(pa[i])+'\n'))
-
-#Write UCAC4 to file (all stars):
-for i in range(0,xs.size):
+        else:
+            myfile.write((str(200)+' '+str(x[i])+' '+str(y[i])+' '+str(m[i])+' ' +\
+                        str(0)+' ' +str(0)+' ' +str(0)+' ' +str(0)+' ' + str(a[i])+' '+\
+                        str(a[i]*ba[i])+' '+str(pa[i])+'\n'))
+    
+    #Write UCAC4 to file (all stars):
+    for i in range(0,xs.size):
             myfile.write((str(100)+' '+str(xs[i])+' '+str(ys[i])+' '+str(ms[i])+'\n'))
-myfile.close()
+    myfile.close()
+
+fname="GOTO1_20170323_00001"
+makelist(180.,30.,fname)
