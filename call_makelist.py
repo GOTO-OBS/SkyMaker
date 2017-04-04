cb3539135019ab35ae1e3845c9d1ed7acff510e5
James Mullaney <Home@dyn007083.shef.ac.uk>
2017-04-04 10:35:33 +0100

    Solved the problem that led it to break when RA>180.

--- call_makelist.py
+++ call_makelist.py
@@ -2,7 +2,7 @@ import numpy as np
 from shutil import copyfile
 from astropy import wcs
 
-from input_skymaker import makelist
+#from input_skymaker import makelist
 
 def mount_pointing(x, y, \
                    width=4., height=4., \
@@ -14,11 +14,20 @@ def mount_pointing(x, y, \
 
     #Use the Haversine formula to calculate
     #where the RA pointing is.
-    d_ra = x*(width - w_olap)
-    theta = np.pi*d_ra/180.
-    ra = 2.*np.arcsin(np.sin(theta/2.)/\
-                      np.cos(np.pi*dec/180.))
-    ra = 180.*ra/np.pi
+
+    #Width of one pointing in radians:
+    theta = np.pi*(width - w_olap)/180.
+    
+    #Delta RA for one pointing:
+    d_ra1 = 2.*np.arcsin(np.sin(theta/2.)/\
+                         np.cos(np.pi*dec/180.))
+
+    #RA of x:
+    ra = 180.*(x*d_ra1)/np.pi
+
+    if ra >= 360:
+        print "x out of range. RA > 360."
+        print "Maximum x for y={} is:".format(y), np.int(np.floor(2.*np.pi/d_ra1))
     
     return ra, dec
 
@@ -39,20 +48,44 @@ def ccd_pointing(ra, dec, \
     
     return ccd_ra, ccd_dec
 
-xs = np.array([30,31,32])
-ys = np.array([2, 3, 4])
+
+#Width and height of each chip in degs:
+xpix=8176
+ypix=6132
+xsi=1.24*xpix/3600.
+ysi=1.24*ypix/3600.
+
+#Width of each pointing (10' overlap between CCDs):
+xpsi = 2.*xsi-(10./60.)
+ypsi = 2.*ysi-(10./60.)
+print xpsi, ypsi
+
+xs = np.array([35,36,37])
+ys = np.array([10, 11, 12])
 i = 0
 for y in ys:
     for x in xs:
         i = i+1
         visit = "{0:04}".format(i)
         
-        mount_ra, mount_dec = mount_pointing(x, y)
-        ccd_ras, ccd_decs = ccd_pointing(mount_ra, mount_dec)
+        #Get (RA,Dec) of mount pointing:
+        #Overlap between pointings is 15':
+        mount_ra, mount_dec = mount_pointing(x, y,\
+                                             width=xpsi, height=ypsi,\
+                                             w_olap=15./60., h_olap=15./60.)
+                                             
+        #Get (RA,Dec) of CCDs:
+        #Overlap between CCDs is 10':
+        ccd_ras, ccd_decs = ccd_pointing(mount_ra, mount_dec,\
+                                         width=xsi, height=ysi,\
+                                         w_olap=10./60., h_olap=10./60.)
+        
+        print mount_ra, mount_dec
+        print ccd_ras, ccd_decs
         for j in np.arange(ccd_ras.size):
             ccd = "{0:02}".format(j+1)
             fname="GOTO_"+ccd+"_20170331_"+visit
-            makelist(ccd_ras[j],ccd_decs[j],fname)
+            #makelist(ccd_ras[j],ccd_decs[j],fname)
             
             with open('goto.conf') as infile, open(str(fname)+'.conf', 'w') as outfile:
                 for line in infile:
